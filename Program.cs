f09e5d772a56008c8996f4ba4ebc30cd5245a91a
golu-10 <15co08@aitdgoa.edu.in>
2021-11-29 12:40:47 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Setting and getting values from variables and reassigning the variable
+
+string firstName;
+firstName = "Bob";
+
+Console.WriteLine(firstName);
+
+Console.WriteLine("After reassigning the values are as follows :");
+
+firstName = "Beth";
+Console.WriteLine(firstName);
+
+firstName = "Conrad";
+Console.WriteLine(firstName);
+
+firstName = "Grant";
+Console.WriteLine(firstName);
+
+//Implicitly local Typed variable
+
+ var message = "Hello World!";
+Console.WriteLine(message);
+
+//Challenege to print the following using 3 variables 
+
+string name = "Bob";
+int v = 3;
+decimal temp = 33.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(v);
+Console.Write(" in your inbox. The temperature is ");
+Console.Write(temp);
+Console.Write(" celsius.");
+Console.Write(name);
+
+
+
+
+
+
